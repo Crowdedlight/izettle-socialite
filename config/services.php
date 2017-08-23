6527a50ba086609529540e4e343d26dbde82cdcd
Crowdedlight <crowdedhd@gmail.com>
2017-08-24 00:46:36 +0200

    added config for services.php

---
+++ config/services.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'izettle' => [
+        'client_id' => env('IZETTLE_CLIENT_ID'),
+        'client_secret' => env('IZETTLE_CLIENT_SECRET'),
+        'redirect' => env('IZETTLE_REDIRECT'),
+    ],
+];
