7be13535df72ab9da12aff18703a68b51ba111fb
Crowdedlight <crowdedhd@gmail.com>
2017-08-23 22:59:51 +0200

    initial commit

---
+++ src/IZettleServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace SocialiteProviders\IZettle;
+
+use Illuminate\Support\ServiceProvider;
+
+class IZettleServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $socialite = $this->app->make('Laravel\Socialite\Contracts\Factory');
+        $socialite->extend(
+            'izettle',
+            function ($app) use ($socialite) {
+                $config = $app['config']['services.izettle'];
+                return $socialite->buildProvider(Provider::class, $config);
+            }
+        );
+    }
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/services.php', 'services'
+        );
+    }
+}
---
+++ src/Provider.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace SocialiteProviders\IZettle;
+
+use Laravel\Socialite\Two\ProviderInterface;
+use SocialiteProviders\Manager\OAuth2\AbstractProvider;
+use SocialiteProviders\Manager\OAuth2\User;
+
+class Provider extends AbstractProvider implements ProviderInterface
+{
+    /**
+     * Unique Provider Identifier.
+     */
+    const IDENTIFIER = 'IZETTLE';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopes = ['READ:PURCHASE'];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase('https://oauth.izettle.net/authorize', $state);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return 'https://oauth.izettle.net/token';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get('https://oauth.izettle.net/users/me', [
+            'headers' => [
+                'Authorization' => 'Bearer '.$token,
+            ],
+        ]);
+
+        return json_decode($response->getBody(), true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map([
+            'id'       => $user['uuid'],
+            'organisation' => $user['organizationUuid'],
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenFields($code)
+    {
+        return array_merge(parent::getTokenFields($code), [
+            'grant_type' => 'authorization_code'
+        ]);
+    }
+}
